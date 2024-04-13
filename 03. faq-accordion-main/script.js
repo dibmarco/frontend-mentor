a5f02f9f365d3b99bcb60a25974794520167ef30
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-04-13 20:00:10 -0300

    Update style and script

--- 03. faq-accordion-main/script.js
+++ 03. faq-accordion-main/script.js
@@ -1,14 +1,23 @@
 "use strict";
 
+const questions = document.querySelectorAll(".question");
+
 const openBtn = document.querySelectorAll(".open-btn");
 const closeBtn = document.querySelectorAll(".close-btn");
 const answers = document.querySelectorAll(".answer");
+
 
 const openClose = function (i) {
   answers[i].classList.toggle("hidden");
   openBtn[i].classList.toggle("btn-hidden");
   closeBtn[i].classList.toggle("btn-hidden");
 };
+
+questions.forEach((question, i) => {
+  question.addEventListener("click", function () {
+    openClose(i);
+  });
+});
 
 openBtn.forEach((btn, i) => {
   btn.addEventListener("click", function () {
