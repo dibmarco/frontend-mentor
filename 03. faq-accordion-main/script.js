844eac2fab580d48997983543726e0314a813991
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-04-16 18:18:33 -0300

    Update html, css, and js

--- 03. faq-accordion-main/script.js
+++ 03. faq-accordion-main/script.js
@@ -6,6 +6,15 @@ const headers = document.querySelectorAll(".q-and-a--header");
 const openBtn = document.querySelectorAll(".open-btn");
 const closeBtn = document.querySelectorAll(".close-btn");
 const answerContainer = document.querySelectorAll(".answer-container");
+const footer = document.querySelector('footer');
+
+
+// check for window width and container heigth to adjust to smaller mobile screens
+const windowWidth = window.innerWidth;
+console.log(windowWidth);
+let containerHeigth = document.querySelector(".container").offsetHeight;
+console.log(containerHeigth);
+
 
 // Immediately set height to 0 to trigger transition
 answerContainer.forEach((el) => {
@@ -29,6 +38,13 @@ headers.forEach((header, i) => {
       )
         ? 0
         : textHeight + "px";
+
+      containerHeigth = document.querySelector(".container").offsetHeight + textHeight;
+      console.log(containerHeigth);
+      // screen width and container heigth check
+      if (windowWidth === 375 && containerHeigth >= 560) {
+        footer.style.display = "none";
+      }
     } else {
       header.classList.add("close");
       openBtn[i].classList.toggle("hidden");
@@ -37,6 +53,13 @@ headers.forEach((header, i) => {
       setTimeout(() => {
         answerContainer[i].classList.toggle("hidden");
       }, 300);
+      const textHeight =
+        answerContainer[i].querySelector(".answer").offsetHeight;
+      containerHeigth = document.querySelector(".container").offsetHeight - textHeight;
+      console.log(containerHeigth);
+      if (windowWidth === 375 && containerHeigth < 560) {
+        footer.style.display = "block";
+      }
     }
   });
 });
