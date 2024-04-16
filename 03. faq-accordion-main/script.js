8fca37a24c37d282323794cf01c0e23ea0fe2edd
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-04-16 18:30:17 -0300

    Update script.js

--- 03. faq-accordion-main/script.js
+++ 03. faq-accordion-main/script.js
@@ -6,15 +6,13 @@ const headers = document.querySelectorAll(".q-and-a--header");
 const openBtn = document.querySelectorAll(".open-btn");
 const closeBtn = document.querySelectorAll(".close-btn");
 const answerContainer = document.querySelectorAll(".answer-container");
-const footer = document.querySelector('footer');
-
+const footer = document.querySelector("footer");
 
 // check for window width and container heigth to adjust to smaller mobile screens
 const windowWidth = window.innerWidth;
-console.log(windowWidth);
+// console.log(windowWidth);
 let containerHeigth = document.querySelector(".container").offsetHeight;
-console.log(containerHeigth);
-
+// console.log(containerHeigth);
 
 // Immediately set height to 0 to trigger transition
 answerContainer.forEach((el) => {
@@ -39,8 +37,9 @@ headers.forEach((header, i) => {
         ? 0
         : textHeight + "px";
 
-      containerHeigth = document.querySelector(".container").offsetHeight + textHeight;
-      console.log(containerHeigth);
+      containerHeigth =
+        document.querySelector(".container").offsetHeight + textHeight;
+      // console.log(containerHeigth);
       // screen width and container heigth check
       if (windowWidth === 375 && containerHeigth >= 560) {
         footer.style.display = "none";
@@ -55,8 +54,9 @@ headers.forEach((header, i) => {
       }, 300);
       const textHeight =
         answerContainer[i].querySelector(".answer").offsetHeight;
-      containerHeigth = document.querySelector(".container").offsetHeight - textHeight;
-      console.log(containerHeigth);
+      containerHeigth =
+        document.querySelector(".container").offsetHeight - textHeight;
+      // console.log(containerHeigth);
       if (windowWidth === 375 && containerHeigth < 560) {
         footer.style.display = "block";
       }
