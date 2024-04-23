27753cb0f9b3c1bec719a11039f062156ccf9c53
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-04-23 13:30:27 -0300

    Update html, css, and js

--- 04. interactive-rating-component-main/script.js
+++ 04. interactive-rating-component-main/script.js
@@ -9,34 +9,32 @@ const submitBtn = document.querySelector(".submit-button");
 
 let rating;
 
-ratingList.addEventListener("click", function (e) {
-  if (e.target.classList.contains("rating-value")) {
-    // Reset styles for all rating values
-    ratingValues.forEach((ratingValue) => {
-      ratingValue.style.backgroundColor = ""; // Set to default or another color
-      ratingValue.style.color = ""; // Set to default or another color
-    });
+function resetRatingStyles() {
+  ratingValues.forEach(ratingValue => {
+    ratingValue.style.backgroundColor = "";
+    ratingValue.style.color = "";
+  });
+}
 
-    // Store rating value
-    rating = e.target.dataset.value;
-    console.log(rating);
-    
-    // Set styles for the clicked element
-    e.target.style.backgroundColor = "var(--orange)";
-    e.target.style.color = "var(--white)";
+function setRatingStyles(element) {
+  element.style.backgroundColor = "var(--orange)";
+  element.style.color = "var(--white)";
+}
+
+ratingList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("rating-value")) {
+    resetRatingStyles(); // Reset styles for all
+    rating = e.target.dataset.value; // Store rating value
+    setRatingStyles(e.target); // Apply styles to the clicked element
   }
 });
 
 submitBtn.addEventListener("click", () => {
-  selectedRating.textContent = rating;
-  card.classList.add("flip");
+  selectedRating.textContent = rating; // Display selected rating
+  card.classList.add("flip"); // Animate card
+  
   setTimeout(() => {
-    card.classList.remove("flip");
-    ratingValues.forEach((ratingValue) => {
-      ratingValue.style.backgroundColor = "";
-      ratingValue.style.color = "";
-    });
-    /* submitBtn.style.backgroundColor = "var(--orange)";
-    submitBtn.style.color = "var(--white)"; */
-  }, 4 * 1000);
+    card.classList.remove("flip"); // Remove animation class
+    resetRatingStyles(); // Reset styles after animation
+  }, 4000); // Corresponds to the duration of the flip animation
 });
