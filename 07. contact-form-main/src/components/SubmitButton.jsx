0d56ed78969d7fad1021badd599bcfab875414fe
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-12-23 11:19:35 -0300

    Update SubmitButton.jsx

--- 07. contact-form-main/src/components/SubmitButton.jsx
+++ 07. contact-form-main/src/components/SubmitButton.jsx
@@ -1,6 +1,6 @@
 function SubmitButton() {
   return (
-    <button className="bg-blue-500 text-white font-semibold py-2 px-4 mt-2 rounded-md shadow-md transition hover:bg-blue-600 active:scale-[.97]">
+    <button className="bg-blue-500 text-white text-lg sm:text-base font-semibold py-2 px-4 mt-2 rounded-md shadow-md transition hover:bg-blue-600 active:scale-[.97]">
       Submit
     </button>
   );
